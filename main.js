fa2cca537c15074ddda005cc59af671a07c401fd
TheSilver1023 <silvergang72@gmail.com>
2023-05-06 16:05:48 -0500

    Testing Version

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
-const { app, BrowserWindow } = require('electron')
+const { dialog } = require('electron').remote;
+const nbt = require('nbtjs');
 
-const createWindow = () => {
-    const window = new BrowserWindow({
-        width: 800,
-        height: 600
+const openButton = document.getElementById('openButton');
+const saveButton = document.getElementById('saveButton');
+const filenameInput = document.getElementById('filename');
+const tagValueInput = document.getElementById('tagValue');
+
+let filename = '';
+
+openButton.addEventListener('click', () => {
+  // Abre un cuadro de diálogo para que el usuario seleccione un archivo NBT.
+  console.error("xd");
+  dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'NBT Files', extensions: ['nbt'] }]
+  }).then(result => {
+    if (result.canceled) return;
+    filename = result.filePaths[0];
+    filenameInput.value = filename;
+    // Lee el archivo NBT y muestra su valor en el campo de entrada.
+    nbt.read(filename, (error, data) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      tagValueInput.value = data.payload.value;
     });
+  });
+});
 
-    window.loadFile('index.html') // ruta de archivo html
-}
-
-app.whenReady().then(() => {
-    createWindow();
+saveButton.addEventListener('click', () => {
+  if (!filename) return;
+  const tag = {
+    type: 'TAG_String',
+    value: tagValueInput.value
+  };
+  const data = {
+    type: 'TAG_Compound',
+    value: {
+      'value': tag
+    }
+  };
+  // Escribe los datos en el archivo NBT.
+  nbt.write(filename, data, (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log('Archivo NBT guardado correctamente');
+  });
 });
